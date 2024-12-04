f7598169914da5c30a7c4c7273c76cc5f1fa4e61
Daniel Fernandez <mdanif9728@gmail.com>
2024-12-04 10:17:57 -0800

    successfully connected to the Mongodb

--- mongo_user.js
+++ mongo_user.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const Data_Base_Uri = process.env.MONGODB_URI
 
-// Create a MOngoClient with a MongoClientOption object to set the Stable Api version.
-const client = new MangoClient(Data_Base_Uri, {
+// Create a MongoClient with a MongoClientOption object to set the Stable Api version.
+const client = new MongoClient(Data_Base_Uri, {
     serverApi: {
-        version: ServerApiVersion.vi,
+        version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
     }
@@ -17,7 +17,7 @@ async function run() {
         await client.connect()
         // Send a ping to confirm a successful connection
         await client.db('admin').command({ ping: 1 })
-        console.log('Pinged your deployment. You successfully connected to MongoDB!')
+        console.log('Pinged your deployment 🎉. You successfully connected to MongoDB!')
     } catch (error) {
         console.error('Error connecting to the Database Client')
     } finally {
