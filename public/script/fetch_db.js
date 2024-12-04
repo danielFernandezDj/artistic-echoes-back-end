10b0a4504d436e0a6a584a67cef40434b561bc6b
Daniel Fernandez <mdanif9728@gmail.com>
2024-12-04 12:31:05 -0800

    creat a www file to handle the http

--- public/script/fetch_db.js
+++ public/script/fetch_db.js
@@ -1,5 +1,8 @@
+const axios = require('axios')
+require('dotenv').config()
+
 const MUSEUM_URL_BASE = axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects')
 
-async function FetchData () {
+async function FetchData() {
     const data = MUSEUM_URL_BASE
 }
